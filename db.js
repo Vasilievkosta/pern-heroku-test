c17329ff58f22d1160645e8eaf914e357aaa3f88
Vasilievkosta <vasilievkosta@gmail.com>
2022-11-02 10:01:39 +0200

    added ssl: to db.js

--- db.js
+++ db.js
@@ -2,20 +2,31 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 // const devConfig = {
-	// user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // host: process.env.DB_HOST,
-    // database: process.env.DB_NAME,
-	// port: process.env.DB_PORT,
+// user: process.env.DB_USER,
+// password: process.env.DB_PASSWORD,
+// host: process.env.DB_HOST,
+// database: process.env.DB_NAME,
+// port: process.env.DB_PORT,
 // };
 
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const proConfig = process.env.DATABASE_URL //heroku addons
 
+// const proConfig = {
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+// };
+
 
 const pool = new Pool({
-	connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig
+    connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
